72795f1e75fe4ae47ae5c60c73482aaaf6c2c662
yango3 <o3.yang@hotmail.com>
2019-03-12 11:54:12 +0800

    frame creation tested

--- test/test.js
+++ test/test.js
@@ -1,3 +1,36 @@
 'use strict'
 
-console.log('test it')
+var LEDP = require('../lib/zkled').LEDPanel
+var FACIL = require('../lib/zkled/facility')
+var DEFLED = require('../lib/zkled/defled')
+
+console.log('test text hex code')
+var buf = FACIL.concatBuffers(DEFLED.GB.temp,
+  DEFLED.GB.comma)
+
+console.log(buf)
+
+buf = FACIL.concatBuffers(
+  DEFLED.GB.winddirection,
+  DEFLED.GB.comma,
+  DEFLED.GB.west
+)
+console.log(buf)
+
+console.log('Test frame creation')
+// 生成一个帧
+var led = new LEDP({})
+console.log('buffer:', DEFLED.GB.txtAhQi)
+led.createTextFrame(DEFLED.GB.txtAhQi, {
+  entry: 0x01,
+  spentry: 0x14,
+  duentry: 0x03E8,
+  exit: 0x0
+})
+
+led.createTextFrame(DEFLED.GB.txtHefi, {
+  entry: 0x02,
+  spentry: 0x02,
+  duentry: 0x00C8,
+  exit: 0x0001
+})
